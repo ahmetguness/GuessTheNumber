d112bd2c054f6e95083d7697ca92dd0613b7d331
ahmet-gunes <ahmetgunes.ceng@gmail.com>
2023-09-01 11:46:27 +0300

    update

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,35 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
-    return <View>
-        <Text>{children}</Text>
-    </View>
+
+    function pressHandler() {
+        console.log("Pressed!!!");
+    };
+
+    return (
+        <Pressable onPress={pressHandler} android_ripple={{ color: '#640233' }} >
+            <View style={styles.container} >
+                <Text style={styles.buttonText}>{children}</Text>
+            </View>
+        </Pressable>
+    );
 }
 
 export default PrimaryButton;
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#72063c',
+        borderRadius: 28,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2,
+        margin: 4,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+    }
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,7 +3,7 @@ import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
     return <View style={styles.inputContainer}>
-        <TextInput />
+        <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" />
         <PrimaryButton>Reset</PrimaryButton>
         <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -17,12 +17,23 @@ const styles = StyleSheet.create({
         padding: 16,
         marginTop: 100,
         marginHorizontal: 24,
-        backgroundColor: '#72063c',
+        backgroundColor: '#4e0329',
         borderRadius: 8,
         elevation: 4, // android only property, others are for ios
         shadowColor: 'black',
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 6,
         shadowOpacity: 0.25,
-    }
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        textAlign: 'center',
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+    },
 });
